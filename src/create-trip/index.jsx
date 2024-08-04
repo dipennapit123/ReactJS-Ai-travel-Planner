8f141805a4995fec57e34ccbd3debb7b0b17fcce
dipennapit123 <dipennapit45@gmail.com>
2024-08-05 04:41:43 +0545

    Implementation GooglePlaceAutoComplete

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
 
 function CreateTrip() {
+  const[place,setPlace]=useState()
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+      <div>
+        <div className='mt-20'>
+          <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+          <GooglePlacesAutocomplete
+          apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+          selectProps={{
+            place,
+            onChange:(v)=>{setPlace(v); console.log(v)}
+
+          }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
